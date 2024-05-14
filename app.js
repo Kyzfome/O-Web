090712a6e4f14b7d5a68644cdd2a129bd7e2e087
Kyzfome2 <domka_201811@gmail.com>
2024-05-14 15:55:45 +0300

    add @media (max-width: 768px)

--- app.js
+++ app.js
@@ -9,21 +9,3 @@ button.addEventListener("click", () => {
   }
 });
 
-const parallax = document.getElementById("parallax");
-
-function moveBackground() {
-  const yPos = window.scrollY;
-  parallax.style.backgroundPositionY = `${yPos * 0.5}px`;
-}
-
-window.addEventListener("scroll", moveBackground);
-
-const textElement = document.getElementById("parallaxText");
-const buttonElement = document.getElementById("parallaxButton");
-
-window.addEventListener("scroll", function () {
-  const scrollValue = window.scrollY;
-
-  textElement.style.transform = `translateY(${scrollValue * -0.5}px)`;
-  buttonElement.style.transform = `translateY(${scrollValue * -0.5}px)`;
-});
